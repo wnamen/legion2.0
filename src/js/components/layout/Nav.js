66118f51c30cbe71a9f68e044601695ca13be5e5
wnamen <williamjosephnamen@gmail.com>
2017-01-03 00:33:45 -0800

    updated modals and reversed credit options

--- src/js/components/layout/Nav.js
+++ src/js/components/layout/Nav.js
@@ -24,10 +24,10 @@ export default class Nav extends React.Component {
                   <Dropdown trigger={
                     <a>12,450 credits <i id ="credit-angle-icon" class="fa fa-angle-down" aria-hidden="true"></i><span id="buy-more" class="electric-blue">Buy More</span></a>
                   }>
-                    <NavItem>50 Credits - $50 <button class="credit-button electric-blue-background white">Buy</button></NavItem>
+                  <NavItem>500 Credits - $400 <button class="credit-button electric-blue-background white">Buy</button></NavItem>
+                  <NavItem>200 Credits - $170 <button class="credit-button electric-blue-background white">Buy</button></NavItem>
                     <NavItem>100 Credits - $90 <button class="credit-button electric-blue-background white">Buy</button></NavItem>
-                    <NavItem>200 Credits - $170 <button class="credit-button electric-blue-background white">Buy</button></NavItem>
-                    <NavItem>500 Credits - $400 <button class="credit-button electric-blue-background white">Buy</button></NavItem>
+                    <NavItem>50 Credits - $50 <button class="credit-button electric-blue-background white">Buy</button></NavItem>
                   </Dropdown>
                 </li>
                 <li class="large-right-margin">
--- src/js/components/search/ActionBar.js
+++ src/js/components/search/ActionBar.js
@@ -1,13 +1,22 @@
 import React from "react";
-import CSSModules from 'react-css-modules';
 import { Dropdown, NavItem, Modal, Input } from "react-materialize";
-import classes from "classnames";
 
-import styles from "./css/actionBar.css";
-import modalStyles from "./modals/modalStyles.css";
-import grid from "../../../css/grid.css";
+import PurchaseModal from "./modals/PurchaseModal"
+import BillingModal from "./modals/BillingModal"
 
 export default class ActionBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentModal: true
+    }
+    this.renderBilling = this.renderBilling.bind(this);
+  }
+
+  renderBilling() {
+    this.setState({ currentModal: !this.state.currentModal })
+  }
+
   render(){
     let data = this.props.results;
     let result_count = data.count;
@@ -18,41 +27,31 @@ export default class ActionBar extends React.Component {
     return(
       <div class="eleven columns">
         <nav class="navbar white-background small-border gray-border">
-          <div id={styles.actionBar} class="nav-wrapper">
+          <div id="actionBar" class="nav-wrapper">
             <ul class="left">
-              <li><span id={styles.resultsTableCount} class="black">Showing {result_count} results</span></li>
+              <li><span id="resultsTableCount" class="black">Showing {result_count} results</span></li>
             </ul>
 
             <ul class="right">
-              <li id={styles.listAdderDropdown} class={styles.rightActions}>
+              <li id="listAdderDropdown" class="rightActions">
                 <Dropdown trigger={
-                  <a>Add to list <i id={styles.listAdderAngleIcon} class="fa fa-angle-down" aria-hidden="true"></i></a>
+                  <a>Add to list <i id="listAdderAngleIcon" class="fa fa-angle-down" aria-hidden="true"></i></a>
                 }>
                 <Modal
-                  class={modalStyles.modal}
                   trigger={
                     <NavItem>My List</NavItem>
                   }>
-                  <div class={classes(grid.sixteen, modalStyles.modalContainer)}>
-                    <img class={modalStyles.image} src="src/img/credit_empty_state.png" />
-                    <hr class={modalStyles.contentHr}/>
-                    <p class={modalStyles.contentHeader}>You don't have enough credits to do that.</p>
-                    <p class={modalStyles.content}>You need 48 more credits</p>
-                    <div  class={modalStyles.creditOptionsDropdown}>
-                      <Dropdown
-                        trigger={
-                          <a>Buy 50 Credits <i class={modalStyles.creditOptionsAngleIcon} class="fa fa-angle-down" aria-hidden="true"></i></a>
-                        }>
-                        <NavItem>50 Credits - $50</NavItem>
-                        <NavItem>100 Credits - $90</NavItem>
-                        <NavItem>200 Credits - $170</NavItem>
-                        <NavItem>500 Credits - $400</NavItem>
-                      </Dropdown>
-                    </div>
-                    <span class={modalStyles.creditDetails}>Your card ending in 3347 </span>
-                    <span class={modalStyles.creditDetails}>will be charged $50</span>
+                  <div class="sixteen modalContainer">
+                    <img class="contentImage" src="src/img/credit_empty_state.png" />
+                    <hr class="contentHr"/>
+                    <p class="contentHeader">You don't have enough credits to do that.</p>
+                    <p class="content">You need 48 more credits</p>
+
+                    { this.state.currentModal === true && <PurchaseModal renderBilling={this.renderBilling} /> }
+                    { this.state.currentModal === false && <BillingModal renderBilling={this.renderBilling} /> }
+
                   </div>
-              </Modal>
+                  </Modal>
                   <NavItem><Input type="text" placeholder="+ Create new list" /></NavItem>
                 </Dropdown>
               </li>
