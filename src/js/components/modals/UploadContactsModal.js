4f013170d20dcd267642d2406517abb3972e9fa3
Jamasen Rodriguez <jamasenr@gmail.com>
2017-01-09 16:38:52 -0800

    contact btn edits

--- src/js/components/modals/UploadContactsModal.js
+++ src/js/components/modals/UploadContactsModal.js
@@ -5,7 +5,12 @@ export default class UploadContactsModal extends React.Component {
   render() {
     return (
         <div class="sixteen modalContainer">
-          Modal Code Here
+         	<div class="eight columns offset-by-two text-center">
+        		<img class="modalIcon smallerIcon" src="/src/img/upload_cloud_asset.png"></img>
+        		<h1 class="modalTitle gray">Almost Done!</h1>
+        		<h5>Please map your contacts to the correct column name</h5>
+		        <div class="lgnBtn settingsBtn lgnBtnLg smoothBkgd electric-blue-background white inline-block signupBtn">Map My Contacts</div>
+        	</div>
         </div>
     )
   }
