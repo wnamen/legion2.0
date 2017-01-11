e028d25e02ba87d034680af1e5f2be59e8aaf6e1
wnamen <williamjosephnamen@gmail.com>
2017-01-10 17:53:59 -0800

    created campaign modal

--- src/js/components/cadence/TemplateViewHeader.js
+++ src/js/components/cadence/TemplateViewHeader.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { Dropdown, NavItem } from "react-materialize";
+import { Modal, Dropdown, NavItem } from "react-materialize";
 import $ from "jquery";
 
 // IMPORT OTHER COMPONENTS AND DEPENDENCIES HERE
+import SaveCampaignModal from "../modals/SaveCampaignModal";
 
 export default class TemplateViewHeader extends React.Component {
   constructor(props) {
@@ -15,13 +16,16 @@ export default class TemplateViewHeader extends React.Component {
 
   render(){
     //RENDER LOGIC HERE
+    const modalTrigger = <div class="lgnBtn smoothBkgd electric-blue-background saveCampaignBtn">Save Campaign</div>;
 
     return(
       <div class="sixteen">
           <div class="gray activeCampaignName">My Campaign</div>
           <div class="topCampaignBtns">
             <div class="lgnBtn smoothBkgd white-background small-border gray-border pauseBtn"><i class="fa fa-pause electric-blue"></i></div>
-            <div class="lgnBtn smoothBkgd electric-blue-background saveCampaignBtn">Save Campaign</div>
+            <Modal trigger={modalTrigger}>
+              <SaveCampaignModal />
+            </Modal>
           </div>
       </div>
 
---
+++ src/js/components/modals/SaveCampaignModal.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Dropdown, NavItem, Button, Modal, Input } from 'react-materialize'
+
+export default class SaveCampaignModal extends React.Component {
+  render() {
+    return (
+        <div class="sixteen modalContainer">
+         	<div class="eight columns text-center smallModal">
+
+
+        	</div>
+        </div>
+    )
+  }
+}
