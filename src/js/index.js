c8a0ac004601fc172e85d1d7332a0ec2111110cc
wnamen <williamjosephnamen@gmail.com>
2017-01-12 18:52:57 -0800

    created company profiles page and renamed profile to people profiles

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,8 @@ import Layout                                       from "./pages/Layout";
 import Landing                                      from "./pages/Landing";
 import Search                                       from "./pages/Search";
 import Cadence                                      from "./pages/Cadence";
-import Profile                                      from "./pages/Profile";
+import PeopleProfiles                               from "./pages/PeopleProfiles";
+import CompanyProfiles                              from "./pages/CompanyProfiles";
 import Settings                                     from "./pages/Settings";
 import Onboarding                                   from "./pages/Onboarding";
 import TOS                                          from "./pages/TOS";
@@ -74,7 +75,8 @@ ReactDOM.render(
       <Route path="/search" name="search" component={Search} onEnter={requireAuth}></Route>
       <Route path="/contacts" name="contacts" component={Contacts} onEnter={requireAuth}></Route>
       <Route path="/campaigns" name="cadence" component={Cadence} onEnter={requireAuth}></Route>
-      <Route path="/profile" name="profile" component={Profile}></Route>
+      <Route path="/people-profiles" name="peopleProfiles" component={PeopleProfiles}></Route>
+      <Route path="/company-profiles" name="companyProfiles" component={CompanyProfiles}></Route>
       <Route path="/settings" name="settings" component={Settings} onEnter={requireAuth}></Route>
       <Route path="/onboarding" name="onboarding" component={Onboarding} onEnter={requireAuth}></Route>
       <Route path="/tos" name="tos" component={TOS}></Route>
