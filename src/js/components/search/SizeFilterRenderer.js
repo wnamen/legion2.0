a8c8f8c44703b0048760877b9ce473749bd41d02
wnamen <williamjosephnamen@gmail.com>
2017-01-27 09:36:56 -0800

    resolved filter bugs

--- src/js/components/search/SizeFilterRenderer.js
+++ src/js/components/search/SizeFilterRenderer.js
@@ -1,16 +1,48 @@
 import React, { Component } from "react";
 
 class SizeFilterRenderer extends Component {
-  
+
   constructor(props){
     super(props);
   }
 
   render(){
-    let data = this.props.value;
+    const less_than = '<';
+    const data = [
+      [0,10],
+      [11,50],
+      [51,200],
+      [201,500],
+      [501,1000],
+      [1001,5000],
+      [5001,10000],
+      [10001]
+    ]
+
+    const results = [
+      "<10",
+      "11-50",
+      "51-200",
+      "201-500",
+      "501-1,000",
+      "1,001-5,000",
+      "5,001-10,000",
+      "10,000+"
+    ]
+
+    const dataPoint = this.props.value;
+    let result;
+
+    data.forEach((v,k) => {
+      if ((v[0] < parseInt(dataPoint)) && ((parseInt(dataPoint) < v[1]))) {
+        return result = results[k];
+      } else if ((v[0] === data[data.length-1][0]) && (v[0] < parseInt(dataPoint))){
+        return result = results[k];
+      }
+    })
 
     return(
-      <span>{ data }</span>
+      <span>{ result }</span>
     )
   }
 }
--- src/js/pages/Search.js
+++ src/js/pages/Search.js
@@ -32,7 +32,6 @@ export default class Search extends React.Component {
     this.setApiState = this.setApiState.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
     this.nextSearch = this.nextSearch.bind(this);
-    this.filterBy = this.filterBy.bind(this);
   }
 
   setApiState() {
@@ -162,58 +161,6 @@ export default class Search extends React.Component {
     });
   }
 
-  // filterBy(data, type){
-  //   const { resultsArray, pureResult } = this.state;
-  //
-  //
-  //     let bySize = data.filter(v => v.name === 'company_size').length ?
-  //
-  //       data.filter(v => v.name === 'company_size').reduce((previousValue, currentValue) => {
-  //
-  //           return pureResult.filter(c =>
-  //             c[currentValue.name] > Number(currentValue.value.split('-')[0]) && c[currentValue.name] < Number(currentValue.value.split('-')[1])
-  //           ).concat(previousValue)
-  //
-  //     }, []) : null;
-  //
-  //     let byRevenue = data.filter(v => v.name === 'revenue').length ?
-  //
-  //       data.filter(v => v.name === 'revenue').reduce((previousValue, currentValue) => {
-  //           let filtered = bySize ? bySize : pureResult;
-  //           let ready = filtered.filter(c =>
-  //             c[currentValue.name] > Number(currentValue.value.split('-')[0]) && c[currentValue.name] < Number(currentValue.value.split('-')[1])
-  //           ).concat(previousValue);
-  //
-  //           bySize = null;
-  //           return ready;
-  //
-  //     }, []) : null;
-  //
-  //     let byFunding = data.filter(v => v.name === 'funding').length ?
-  //
-  //       data.filter(v => v.name === 'funding').reduce((previousValue, currentValue) => {
-  //           let filtered = !byRevenue ? bySize ? bySize : pureResult : byRevenue ;
-  //
-  //           let ready = filtered.filter(c =>
-  //             c[currentValue.name] > Number(currentValue.value.split('-')[0]) && c[currentValue.name] < Number(currentValue.value.split('-')[1])
-  //           ).concat(previousValue);
-  //
-  //           bySize = null;
-  //           byRevenue = null;
-  //           return ready;
-  //
-  //     }, []) : null;
-  //
-  //   const filteredCheck = [bySize, byRevenue, byFunding].filter(v => v)[0];
-  //   let filtered = !data.length ? pureResult : filteredCheck;
-  //
-  //   this.setState({
-  //     resultsArray: filtered
-  //   })
-  //
-  //   this.forceUpdate();
-  // };
-
   closeNotification = () => {
     this.setState({notification: false})
   }
@@ -232,7 +179,6 @@ export default class Search extends React.Component {
             apiState={apiState}
             userToken={token}
             setApiState={this.setApiState}
-            filterBy={this.filterBy}
             onSearchChange={this.handleSearch}
             onInterestSearch={this.handleInterestSearch}
           />
